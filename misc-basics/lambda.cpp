94c9b1694d2115ab76710f77b730287171fdbcb4
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2018-06-13 18:25:58 -0300

    ..

--- misc-basics/lambda.cpp
+++ misc-basics/lambda.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
+auto lambda = [](auto x, auto y) -> auto {return x + y;};
+
+
 int main() {
+        printf("arrancando\n\r");
+        auto x = 4;
+        auto y = [&r = x, x = x+1](int a, int b)->int {
+                    printf("no soy el primer texto que sale\n\r");
+                    r += 2;
+                    return x+2;
+                };  // Updates ::x to 6, and initializes y to 7.
 
-auto x = 4;
-auto y = [&r = x, x = x+1]()->int {
-            r += 2;
-            return x+2;
-         }();  // Updates ::x to 6, and initializes y to 7.
+        auto a = 1;
+        auto b = 1;
+        //printf("a=%d, b=%d\n\r", ++a, a);
+        //printf("a=%d, b=%d\n\r", b, ++b);
+        
+        printf("y=%d, x=%d\n\r", y(1, 2), x);
 
+        printf("y=%d, x=%d\n\r", y(2, 3), x);
 
-printf("x=%d, y=%d\n\r", x, y);
+        printf("%d", lambda(1, 2));
+
+       
 }
